4cf7d1958ac75dcfcada52b4a4217572cf2a063a
shahramsamar <shahramsamar2010@gmail.com>
2024-12-22 22:45:42 +0330

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -119,15 +119,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+# Media files settings
+MEDIA_URL = '/media/'  # URL for serving media files
+MEDIA_ROOT = BASE_DIR / 'media'  # Directory to store uploaded media files
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# Static files settings
+STATIC_URL = '/static/'  # URL to access static files
+STATIC_ROOT = BASE_DIR / 'static'  # Directory where 'collectstatic' collects static files for production
 
-STATICFILES_DIR =[
-    BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'staticfiles',  # Additional directory for custom static files during development
 ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
