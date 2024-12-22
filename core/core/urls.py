ae4b5074e3a49a23c68770bcbd03ee0934be2228
shahramsamar <shahramsamar2010@gmail.com>
2024-12-22 22:45:27 +0330

    Update urls.py

--- core/core/urls.py
+++ core/core/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/',include('rest_framework.urls')),
     path('accounts/',include('django.contrib.auth.urls')),
-    path('blog/',include('blog.urls')),
+    path('',include('blog.urls')),
     path('api-docs/',include_docs_urls(title="api-sample")),
     
 ]
