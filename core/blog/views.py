4366117dccbae7a2560d5575d779d119b26d889e
shahramsamar <shahramsamar2010@gmail.com>
2024-12-23 13:23:19 +0330

    Update views.py

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,12 +1,102 @@
 from typing import Any
 from django.db.models.query import QuerySet
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from django.views.generic.base import TemplateView, RedirectView
 from django.views.generic import ListView, DetailView, FormView, CreateView, UpdateView, DeleteView
 from blog.models import Post
 from blog.form import ContactForm, PostForm
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.http import HttpResponse
+from django.views import View
+from accounts.models import Profile
+
+
+
+class PostListView(PermissionRequiredMixin, LoginRequiredMixin, ListView):
+    
+    '''
+    a class  based listview to show post_list page
+    '''
+    # this two command to way for  get object all 
+    permission_required = "blog.view_post"
+    
+    model = Post
+    # queryset = Post.objects.all()
+    template_name = "blog/post_list.html"  
+
+    # change name for object_list and choses your opinion name
+    context_object_name = 'posts'
+    paginate_by = 3
+    # if we added ordering must changing  structure
+    ordering = '-id'
+    
+    # disable when ordering use
+    # def get_queryset(self):
+    #     posts = Post.objects.filter(status=True)
+    #     return posts
+ 
+ 
+class PostDetailView(LoginRequiredMixin, DetailView):
+    '''
+    a class  based DetailView to show post_detail page
+    '''
+    model = Post    
+    template_name = "blog/post_detail.html"   
+
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    '''
+    a class  based CreateView to show post_form page
+    '''
+    model = Post 
+    # form_class = PostForm
+    fields = ['title','content','status','category','published_date']
+    success_url = '/'
+    template_name = "blog/post_form.html" 
+
+    # get user login and filed author object
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        profile = get_object_or_404(Profile, user=self.request.user)
+        form.instance.author = profile
+        self.object.save()
+        return super().form_valid(form)
+    
+    
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    '''
+    a class  based UpdateView to show post_form page
+    '''  
+    model = Post 
+    form_class = PostForm
+    success_url = '/'
+    template_name = "blog/post_form.html" 
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    '''
+    a class  based DeleteView to show post_form page
+    '''  
+    model = Post 
+    success_url = '/'
+    template_name = "blog/post_confirm_delete.html" 
+
+
+class PostDoneView(LoginRequiredMixin, View):  
+    '''
+    a class  based DoneView to done post in page
+    '''  
+    model = Post
+    success_url = "/"
+    def get(self,request,*args,**kwargs):
+        # print('post id :',kwargs["pk"])
+        post = get_object_or_404(Post,pk=kwargs['pk'])
+        post.status = True
+        post.save()
+        # print('post status update')
+        return redirect(self.success_url)
+  
 '''
 fbv for templateview
 '''
@@ -48,40 +138,14 @@ a class  based redirectview to show redirect_to_maktabkhooneh page
 #         return super().get_redirect_url(*args, **kwargs)
 
 
-class PostListView(PermissionRequiredMixin, LoginRequiredMixin, ListView):
-    
-    '''
-    a class  based listview to show post_list page
-    '''
-    # this two command to way for  get object all 
-    permission_required = "blog.view_post"
-    
-    model = Post
-    # queryset = Post.objects.all()
-    
-    # change name for object_list and choses your opinion name
-    context_object_name = 'posts'
-    paginate_by = 2
-    # if we added ordering must changing  structure
-    ordering = '-id'
-    
-    # disable when ordering use
-    # def get_queryset(self):
-    #     posts = Post.objects.filter(status=True)
-    #     return posts
- 
- 
-class PostDetailView(LoginRequiredMixin, DetailView):
-    '''
-    a class  based DetailView to show post_detail page
-    '''
-    model = Post    
-    
+
+
+
 
 # class PostCreateView(FormView):
-    '''
-    a class  based FormView to show post_create page
-    '''
+
+    #a class  based FormView to show post_create page
+
     # template_name = 'blog/contact.html'
     # # f    # form_class = ContactForm
     # form_class = PostForm
@@ -92,42 +156,12 @@ class PostDetailView(LoginRequiredMixin, DetailView):
     #     return super().form_valid(form)   
 
 
-class PostCreateView(LoginRequiredMixin, CreateView):
-    '''
-    a class  based CreateView to show post_form page
-    '''
-    model = Post 
-    # form_class = PostForm
-    fields = ['title','content','status','category','published_date']
-    success_url = '/blog/post/'
-    
-    # get user login and filed author object
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-    
-    
-class PostEditView(LoginRequiredMixin, UpdateView):
-    '''
-    a class  based UpdateView to show post_form page
-    '''  
-    model = Post 
-    form_class = PostForm
-    success_url = '/blog/post/'
-
-class PostDeleteView(LoginRequiredMixin, DeleteView):
-    '''
-    a class  based DeleteView to show post_form page
-    '''  
-    model = Post 
-    success_url = '/blog/post/'
 
     
-    
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
 
-@api_view()
-def ApiPostListView(request):
-        return Response("ok")
+# @api_view()
+# def ApiPostListView(request):
+#         return Response("ok")
         
