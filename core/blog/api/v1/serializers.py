deecd7036534953d4145dd356a787eea5b1a4add
shahramsamar <shahramsamar2010@gmail.com>
2024-12-23 14:26:41 +0330

    Update serializers.py

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -17,11 +17,13 @@ class  CategorySerializer(serializers.ModelSerializer):
     
 class  PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source="get_snippet")
-    relative_url = serializers.URLField(source="get_absolute_api_url", read_only=True)
+    relative_url = serializers.URLField(source="get_absolute_api_url",
+                                         read_only=True)
     absolute_url = serializers.SerializerMethodField()
-    category = serializers.SlugRelatedField(many=False,
-                                            slug_field="name", queryset=Category.objects.all())
-    # category = CategorySerializer()
+    # category = serializers.SlugRelatedField(many=False,
+    #                                         slug_field="name", 
+    #                                         queryset=Category.objects.all())
+    category = CategorySerializer()
     
     
     class Meta:
@@ -37,12 +39,14 @@ class  PostSerializer(serializers.ModelSerializer):
     def get_absolute_url(self, obj):
         request = self.context.get("request")
         return request.build_absolute_uri(obj.pk)
+    
     #when we need to change data for show #
     def to_representation(self, instance):
         """
         Customize the data representation based on whether it's a single post or a list view.
         """
         request = self.context.get("request")
+        # print(request.__dict__)
         rep =  super().to_representation(instance)
         
         # rep['state'] = 'list'
@@ -50,15 +54,16 @@ class  PostSerializer(serializers.ModelSerializer):
         #     rep['state'] = 'single'
         if request.parser_context.get("kwargs").get('pk'):
             # Single post view
-            rep.pop('snippet')
-            rep.pop('relative_url')
-            rep.pop('absolute_url')
+            rep.pop('snippet', None)
+            rep.pop('relative_url', None)
+            rep.pop('absolute_url', None)
         else:
             # List view
             rep.pop('content', None)
             
         # Add detailed category data
-        rep['category'] = CategorySerializer(instance.category, context={'request':request}).data
+        rep['category'] = CategorySerializer(instance.category,
+                                              context={'request':request}).data
         return rep
     
         # def create(self, validated_data):
