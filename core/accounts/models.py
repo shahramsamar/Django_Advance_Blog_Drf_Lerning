5c06eb322e17d1fe39c1b71059d88919fd81f3a6
shahramsamar <shahramsamar2010@gmail.com>
2024-12-23 13:23:48 +0330

    Update models.py

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -89,9 +89,15 @@ class Profile(models.Model):
     
     def __str__(self):
         return self.user.email
-    
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
 @receiver(post_save, sender =User)   
 def save_profile(sender, instance, created,  **kwargs):
     if created:
         Profile.objects.create(user=instance)
-    
+    # instance.profile.save()
