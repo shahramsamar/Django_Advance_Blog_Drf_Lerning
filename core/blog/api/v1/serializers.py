44195327b81b4328d28f08acb25a1ba59a6fba35
shahramsamar <shahramsamar2010@gmail.com>
2024-12-23 23:31:43 +0330

    Update serializers.py

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -20,12 +20,13 @@ class  PostSerializer(serializers.ModelSerializer):
     relative_url = serializers.URLField(source="get_absolute_api_url",
                                          read_only=True)
     absolute_url = serializers.SerializerMethodField()
-    category = serializers.SlugRelatedField(many=False,
-                                            slug_field="name", 
-                                            queryset=Category.objects.all())
+    # category = serializers.SlugRelatedField(many=False,
+                                            # slug_field="name", 
+                                            # queryset=Category.objects.all())
     # category = CategorySerializer()
     
-    
+
+
     class Meta:
         model = Post
         # fields = "__all__"
@@ -34,19 +35,18 @@ class  PostSerializer(serializers.ModelSerializer):
         # read_only_fields = ['author', 'created_date', 'updated_date']
         read_only_fields = ['author']
     
+    # def create(self, validated_data):
+    #         user = self.context['request'].user
+    #         try:
+    #             profile = Profile.objects.get(user=user)  # Get the user's profile
+                 
+    #             validated_data['author'] = profile  # Assign the profile as the author
+    #             return super().create(validated_data)
+    #         except Profile.DoesNotExist:
+    #             raise serializers.ValidationError("The authenticated user does not have an associated profile.")
+           
 
-    def create(self, validated_data):
-        #     """
-        #     Automatically set the author field to the currently authenticated user's profile.
-        #     """
-            # validated_data['author '] = Profile.objects.get(user = self.context.get('request').user.id)
-        #     # validated_data['author'] = Profile.objects.get(user__id = self.context.get('request').user.id)
-            # return super().create(validated_data)
-            
-            user = self.context['request'].user
-            profile = Profile.objects.get(user=user)
-            validated_data['author'] = profile  # Corrected key
-            return super().create(validated_data)
+        
 
     def get_absolute_url(self, obj):
         request = self.context.get("request")
@@ -77,17 +77,17 @@ class  PostSerializer(serializers.ModelSerializer):
         rep['category'] = CategorySerializer(instance.category,
                                               context={'request':request}).data
         return rep
-    
+    def create(self, validated_data):
+            """
+            Automatically set the author field to the currently authenticated user's profile.
+            """
+            #   validated_data['author '] = Profile.objects.get(user = self.context.get('request').user.id)
+            #   validated_data['author'] = Profile.objects.get(user__id = self.context.get('request').user.id)
+            #   return super().create(validated_data)
+            
+            user = self.context['request'].user
+            profile = Profile.objects.get(user=user)
+            validated_data['author'] = profile  # Corrected key
+            return super().create(validated_data)
    
             
-        # def create(self, validated_data):
-        #     user = self.context['request'].user
-        #     try:
-        #         profile = Profile.objects.get(user=user)  # Get the user's profile
-        #     except Profile.DoesNotExist:
-        #         raise serializers.ValidationError("The authenticated user does not have an associated profile.")
-            
-        #     validated_data['author'] = profile  # Assign the profile as the author
-        #     return super().create(validated_data)
-
-        
