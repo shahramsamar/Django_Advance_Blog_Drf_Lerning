b7191c0ffaf66c8b34ab7a420d62da89955dcf84
shahram <138889794+shahramsamar@users.noreply.github.com>
2025-01-01 12:26:58 +0330

    added api-v1 in accounts app

---
+++ core/accounts/api/v1/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path, include
+from blog import views
+
+
+app_name = 'api-v1'
+
+urlpatterns = [
+
+]
---
+++ core/accounts/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path, include
+from blog import views
+
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('',include('django.contrib.auth.urls')),
+    path('api/v1/', include('accounts.api.v1.urls')),
+
+]
