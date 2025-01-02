d8bbbcf85fd2cea869a0663a89ad16cae2ca5ded
shahram <138889794+shahramsamar@users.noreply.github.com>
2025-01-02 17:55:17 +0330

    Update urls.py

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,10 +1,16 @@
 
 from django.urls import path, include
-from blog import views
+from blog.api.v1 import views
 
 
 app_name = 'api-v1'
 
 urlpatterns = [
+    # registration
+    path('registration/', views.RegistrationApiView.as_view(),'registrations'),
+    # change password
+    # reset password
+    # login token
+    # login jwt token
 
 ]
