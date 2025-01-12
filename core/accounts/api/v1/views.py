831117b294ed723c93d00169576fc0d0c04f3898
shahram <138889794+shahramsamar@users.noreply.github.com>
2025-01-12 13:01:28 +0330

    TokenAuthentication

--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -16,5 +16,14 @@ class RegistrationApiView(generics.GenericAPIView):
             data ={
                 'email': serializer.validated_data['email']
             }
+            # return Response(serializer.data,status=status.HTTP_201_CREATED)
             return Response(data,status=status.HTTP_201_CREATED)
+        # The line `return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)` is
+        # returning a response with the validation errors from the serializer in case the data
+        # provided in the request is not valid.
+        # This line of code is returning a response with the validation errors from the serializer in
+        # case the data provided in the request is not valid. The `serializer.errors` contains a
+        # dictionary of field-level errors that occurred during validation. By returning this response
+        # with a status of `HTTP_400_BAD_REQUEST`, it indicates that the request data was not valid
+        # and provides the client with information about the specific validation errors that occurred.
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
--- core/core/settings.py
+++ core/core/settings.py
@@ -41,8 +41,10 @@ INSTALLED_APPS = [
     'accounts',
     'blog', 
     'rest_framework',
+    'rest_framework.authtoken',
     'django_filters',
     'drf_yasg',
+    
 
 ]
 MIDDLEWARE = [
@@ -156,6 +158,10 @@ REST_FRAMEWORK = {
                   # public pagination
                 #   'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
                 #   'PAGE_SIZE': 100
+                'DEFAULT_AUTHENTICATION_CLASSES': [
+                'rest_framework.authentication.BasicAuthentication',
+                'rest_framework.authentication.SessionAuthentication',
+                'rest_framework.authentication.TokenAuthentication',] 
                   
-                  
-                  }
+}
+
