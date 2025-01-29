bc49592628e51055f9f1bb00ad78637e38e4a956
shahram <shahramsamar2010@gmail.com>
2025-01-29 21:42:04 +0330

    Update urls.py
    login with ObtainAuthToken

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,13 +1,13 @@
-
 from django.urls import path, include
 from accounts.api.v1 import views
-
+from rest_framework.authtoken.views import ObtainAuthToken
 
 app_name = 'api-v1'
 
 urlpatterns = [
     # registration
     path('registration/', views.RegistrationApiView.as_view(),name='registrations'),
+    path('token/login',ObtainAuthToken.as_view(),name='token-login')
     # change password
     # reset password
     # login token
